c856b1bce8acfebbb60bb59d2d86c71ea7534fee
Jleagle <jimeagle@gmail.com>
2020-12-28 16:09:55 +0000

    Fix logging in

--- go.mod
+++ go.mod
@@ -5,5 +5,10 @@ go 1.15
 require (
 	github.com/chromedp/cdproto v0.0.0-20201204063249-be40c824ad18
 	github.com/chromedp/chromedp v0.5.4
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/robfig/cron v1.2.0
+	github.com/robfig/cron/v3 v3.0.1
+	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.16.0
+	golang.org/x/sys v0.0.0-20201223074533-0d417f636930 // indirect
 )
--- main.go
+++ main.go
@@ -2,32 +2,26 @@ package main
 
 import (
 	"context"
-	"github.com/chromedp/cdproto/runtime"
-	"io/ioutil"
-	"log"
+	"os"
 	"regexp"
-	"strings"
 	"time"
 
 	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/cdproto/network"
+	"github.com/chromedp/cdproto/runtime"
 	"github.com/chromedp/chromedp"
 	"github.com/chromedp/chromedp/device"
+	"github.com/robfig/cron/v3"
 	"go.uber.org/zap"
 )
 
-const (
-	username    = ""
-	password    = ""
-	membersPath = "https://www.puregym.com/members/"
+var (
+	regexMembers = regexp.MustCompile(`(?i)([0-9]{1,3}) of ([0-9]{1,3})`)
+	cookies      []*network.Cookie
+	logger       *zap.SugaredLogger
 )
 
-var regexMembers = regexp.MustCompile(`([0-9]{1,3}) (of|OF) ([0-9]{1,3})`)
-var cookies []*network.Cookie
-var logger *zap.SugaredLogger
-
 func init() {
-
 	loggerDev, _ := zap.NewDevelopment()
 	logger = loggerDev.Sugar()
 }
@@ -36,141 +30,128 @@ func main() {
 
 	defer logger.Sync()
 
+	trigger()
+
+	c := cron.New()
+	_, err := c.AddFunc("*/10 * * * *", trigger)
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+	c.Start()
+
+	<-make(chan struct{})
+}
+
+func trigger() {
+
 	ctx := context.Background()
 
 	ctx, cancel1 := chromedp.NewContext(ctx)
 	defer cancel1()
 
-	ctx, cancel2 := context.WithTimeout(ctx, 5*time.Second)
+	ctx, cancel2 := context.WithTimeout(ctx, 30*time.Second)
 	defer cancel2()
 
-	listenForNetworkEvent(ctx, logger)
-
-	// people, town, err := checkMembers(ctx)
-	// if errors.Is(err, context.DeadlineExceeded) {
-	// 	logger.Error(err)
 	people, town, err := loginAndCheckMembers(ctx)
 	if err != nil {
 		logger.Error(err)
-	}
-	// }
-
-	if err := ioutil.WriteFile("screenshot1.png", b1, 0644); err != nil {
-		log.Fatal(err)
 	}
 
 	logger.Info("people ", people)
 	logger.Info("town ", town)
 }
 
-func checkMembers(ctx context.Context) (people, town string, err error) {
+func loginAndCheckMembers(ctx context.Context) (people, town string, err error) {
 
 	actions := []chromedp.Action{
 		network.Enable(),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			if len(cookies) > 0 {
+				logger.Info("Setting cookies")
+			}
+			for _, v := range cookies {
+				expr := cdp.TimeSinceEpoch(time.Unix(int64(v.Expires), 0))
+				_, err := network.SetCookie(v.Name, v.Value).
+					WithExpires(&expr).
+					WithDomain(v.Domain).
+					WithHTTPOnly(v.HTTPOnly).
+					WithPath(v.Path).
+					WithPriority(v.Priority).
+					WithSameSite(v.SameSite).
+					WithSecure(v.Secure).
+					Do(ctx)
+
+				if err != nil {
+					return err
+				}
+			}
+			return nil
+		}),
 		chromedp.Emulate(device.IPadPro),
-		chromedp.ActionFunc(setCookies),
-		chromedp.Navigate(membersPath),
+		chromedp.Navigate("https://www.puregym.com/members/"),
+		chromedp.WaitVisible("#loginForm, #people_in_gym"),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+
+			// Accept cookies
+			var cookieNodes []*cdp.Node
+			err = chromedp.Nodes("button.coi-banner__accept", &cookieNodes, chromedp.AtLeast(0)).Do(ctx)
+			if err != nil {
+				return err
+			}
+
+			if len(cookieNodes) > 0 {
+
+				logger.Info("Submitting cookie popup")
+				_, exp, err := runtime.Evaluate("CookieInformation.submitAllCategories();").Do(ctx)
+				if err != nil {
+					return err
+				}
+				if exp != nil {
+					return exp
+				}
+			}
+
+			// Login
+			var loginNodes []*cdp.Node
+			err = chromedp.Nodes("#loginForm", &loginNodes, chromedp.AtLeast(0)).Do(ctx)
+			if err != nil {
+				return err
+			}
+
+			if len(loginNodes) > 0 {
+
+				logger.Info("Logging in")
+
+				err = chromedp.SendKeys("#loginForm input[type=email]", os.Getenv("PURE_USER")).Do(ctx)
+				if err != nil {
+					return err
+				}
+
+				err = chromedp.SendKeys("#loginForm input[type=password]", os.Getenv("PURE_PASS")).Do(ctx)
+				if err != nil {
+					return err
+				}
+
+				err = chromedp.Click("#login-submit", chromedp.ByID).Do(ctx)
+				if err != nil {
+					return err
+				}
+			}
+
+			return nil
+		}),
 		chromedp.WaitVisible("#people_in_gym"),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			logger.Info("Logged in, taking cookies")
+			var err error
+			cookies, err = network.GetAllCookies().Do(ctx)
+			return err
+		}),
 		chromedp.InnerHTML("#people_in_gym span", &people),
 		chromedp.InnerHTML("#people_in_gym a", &town),
 	}
 
 	err = chromedp.Run(ctx, actions...)
 	return people, town, err
-}
-
-var b1 []byte
-
-func loginAndCheckMembers(ctx context.Context) (people, town string, err error) {
-
-	actions := []chromedp.Action{
-		network.Enable(),
-		chromedp.Emulate(device.IPadPro),
-		chromedp.Navigate(membersPath),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-
-			_, exp, err := runtime.Evaluate("CookieInformation.submitAllCategories();").Do(ctx)
-			if err != nil {
-				return err
-			}
-			if exp != nil {
-				return exp
-			}
-			return nil
-		}),
-		chromedp.WaitVisible("#loginForm"),
-		chromedp.SendKeys("#loginForm input[type=email]", ""), // todo
-		chromedp.SendKeys("#loginForm input[type=password]", ""), // todo
-		chromedp.Submit("#loginForm"),
-		chromedp.CaptureScreenshot(&b1),
-
-		// CookieInformation.submitAllCategories();
-
-		// chromedp.Click("#loginForm button[type=submit]"),
-		// chromedp.Sleep(time.Second * 2),
-		// chromedp.WaitVisible("#people_in_gym"),
-		// chromedp.CaptureScreenshot(&b1),
-		// chromedp.InnerHTML("#people_in_gym span", &people),
-		// chromedp.InnerHTML("#people_in_gym a", &town),
-		// chromedp.ActionFunc(getCookies),
-	}
-
-	err = chromedp.Run(ctx, actions...)
-	return people, town, err
-}
-
-func getCookies(ctx context.Context) error {
-
-	var err error
-	cookies, err = network.GetAllCookies().Do(ctx)
-	for _, v := range cookies {
-		logger.Info(v)
-	}
-
-	return err
-}
-
-func setCookies(ctx context.Context) error {
-
-	for _, v := range cookies {
-
-		expr := cdp.TimeSinceEpoch(time.Unix(int64(v.Expires), 0))
-
-		_, err := network.SetCookie(v.Name, v.Value).
-			WithExpires(&expr).
-			WithDomain(v.Domain).
-			WithHTTPOnly(v.HTTPOnly).
-			WithPath(v.Path).
-			WithPriority(v.Priority).
-			WithSameSite(v.SameSite).
-			WithSecure(v.Secure).
-			Do(ctx)
-
-		if err != nil {
-			logger.Error(err)
-		}
-	}
-
-	return nil
-}
-
-// var r1 = regexp.MustCompile(`(?i)/(Login|members)/`)
-
-func listenForNetworkEvent(ctx context.Context, logger *zap.SugaredLogger) {
-	chromedp.ListenTarget(ctx, func(ev interface{}) {
-
-		switch ev := ev.(type) {
-		case *network.EventResponseReceived:
-
-			// if r1.MatchString(ev.Response.URL) {
-			if strings.Contains(ev.Response.URL, "www.puregym.com/") {
-
-				// logger.Info("network: ", ev.Response.URL)
-
-				// val, ok := ev.Response.Headers["set-cookie"]
-				// logger.Info(ok, val)
-
-			}
-		}
-	})
 }
