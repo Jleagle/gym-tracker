9eb5db883c98045904ca6d6157cc3c4ab5b89236
Jleagle <jimeagle@gmail.com>
2020-12-28 22:10:59 +0000

    Tidy

--- main.go
+++ main.go
@@ -85,9 +85,9 @@ func loginAndCheckMembers(ctx context.Context) (people, town string, err error) 
 	actions := []chromedp.Action{
 		network.Enable(),
 		chromedp.ActionFunc(func(ctx context.Context) error {
-			if len(cookies) > 0 {
-				logger.Info("Setting cookies")
-			}
+
+			logger.Info("Setting cookies", zap.Int("count", len(cookies)))
+
 			for _, v := range cookies {
 				expr := cdp.TimeSinceEpoch(time.Unix(int64(v.Expires), 0))
 				_, err := network.SetCookie(v.Name, v.Value).
@@ -104,6 +104,7 @@ func loginAndCheckMembers(ctx context.Context) (people, town string, err error) 
 					return err
 				}
 			}
+
 			return nil
 		}),
 		chromedp.Emulate(device.IPadPro),
@@ -111,7 +112,7 @@ func loginAndCheckMembers(ctx context.Context) (people, town string, err error) 
 		chromedp.WaitVisible("#loginForm, #people_in_gym"),
 		chromedp.ActionFunc(func(ctx context.Context) error {
 
-			// Accept cookies
+			// Accept cookies, probably don't need to bother
 			var cookieNodes []*cdp.Node
 			err = chromedp.Nodes("button.coi-banner__accept", &cookieNodes, chromedp.AtLeast(0)).Do(ctx)
 			if err != nil {
@@ -161,7 +162,9 @@ func loginAndCheckMembers(ctx context.Context) (people, town string, err error) 
 		}),
 		chromedp.WaitVisible("#people_in_gym"),
 		chromedp.ActionFunc(func(ctx context.Context) error {
+
 			logger.Info("Logged in, taking cookies")
+
 			var err error
 			cookies, err = network.GetAllCookies().Do(ctx)
 			return err
