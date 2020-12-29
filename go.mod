0372161a0a63d2956110a6c99870c0702e34fc23
Jleagle <jimeagle@gmail.com>
2020-12-29 19:39:32 +0000

    Setup templates

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/chromedp/cdproto v0.0.0-20201204063249-be40c824ad18
 	github.com/chromedp/chromedp v0.5.4
 	github.com/go-chi/chi v1.5.1
+	github.com/gobuffalo/packr/v2 v2.8.1
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/robfig/cron v1.2.0
 	github.com/robfig/cron/v3 v3.0.1
--- webserver.go
+++ webserver.go
@@ -4,10 +4,18 @@ import (
 	"compress/flate"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/gobuffalo/packr/v2"
 	"go.uber.org/zap"
+	"html/template"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
+)
+
+var (
+	assetBox = packr.New("assets", "./assets")
 )
 
 func webserver() {
@@ -15,6 +23,8 @@ func webserver() {
 	r := chi.NewRouter()
 	r.Use(middleware.RedirectSlashes)
 	r.Use(middleware.NewCompressor(flate.DefaultCompression, "text/html", "text/css", "text/javascript", "application/json", "application/javascript").Handler)
+
+	r.Get("/assets/*", assetHandler(assetBox, "/assets"))
 
 	r.Get("/", homeHandler)
 	r.Get("/{gym}", homeHandler)
@@ -29,7 +39,7 @@ func webserver() {
 		ReadHeaderTimeout: 2 * time.Second,
 	}
 
-	logger.Info("Starting Frontend on " + "http://" + s.Addr)
+	logger.Info("Starting Frontend on " + "http://localhost:" + os.Getenv("PURE_PORT"))
 
 	err := s.ListenAndServe()
 	if err != nil {
@@ -43,15 +53,72 @@ type homeTemplate struct {
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 
-	w.Write([]byte("home"))
+	w.Header().Set("Content-Type", "text/html")
+
+	data := homeTemplate{
+		Gym: chi.URLParam(r, "gym"),
+	}
+
+	err := template.
+		Must(template.ParseFiles("./assets/home.gohtml")).
+		ExecuteTemplate(w, "home", data)
+
+	if err != nil {
+		logger.Error("executing template", zap.Error(err))
+	}
+}
+
+type errorTemplate struct {
+	Code int
 }
 
 func errorHandler(w http.ResponseWriter, r *http.Request) {
 
-	w.Write([]byte("error"))
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(404)
+
+	data := errorTemplate{
+		Code: 404,
+	}
+
+	err := template.
+		Must(template.ParseFiles("./assets/error.gohtml")).
+		ExecuteTemplate(w, "home", data)
+
+	if err != nil {
+		logger.Error("executing template", zap.Error(err))
+	}
 }
 
 func newGymHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("new gym"))
+}
+
+func assetHandler(box *packr.Box, path string) func(w http.ResponseWriter, r *http.Request) {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		b, err := box.Find(strings.TrimPrefix(r.URL.Path, path))
+		if err != nil {
+			errorHandler(w, r)
+			return
+		}
+
+		types := map[string]string{
+			".js":  "text/javascript",
+			".css": "text/css",
+			".png": "image/png",
+			".jpg": "image/jpeg",
+		}
+
+		if val, ok := types[filepath.Ext(r.URL.Path)]; ok {
+			w.Header().Add("Content-Type", val)
+		}
+
+		_, err = w.Write(b)
+		if err != nil {
+			logger.Error("writing asset to response", zap.Error(err))
+		}
+	}
 }
