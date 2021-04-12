5b5955d9f2e5a2a205e30ab95b20e18462add70f
James Eagle <jimeagle@gmail.com>
2021-04-12 23:13:43 +0100

    heatmap endpoint

--- webserver.go
+++ webserver.go
@@ -45,11 +45,73 @@ func webserver() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
+	app.Get("/heatmap.json", heatmapHandler)
 
 	err := app.Listen("0.0.0.0:" + os.Getenv("PURE_PORT"))
 	if err != nil {
 		logger.Error("serving webserver", zap.Error(err))
 	}
+}
+
+func heatmapHandler(c *fiber.Ctx) error {
+
+	q := influxquerybuilder.New().
+		Select(`max("max") AS max_max`).
+		Select(`max("pcnt") AS max_pcnt`).
+		Select(`max("people") AS max_people`).
+		From(`PureGym"."alltime"."gyms`).
+		Where(`time`, `>`, `now()-1d`).
+		Where(`gym`, `=`, `Fareham`).
+		GroupByTime(influxquerybuilder.NewDuration().Minute(10)).
+		Fill("null")
+
+	resp, err := influx.Read(q.Build())
+	if err != nil {
+		logger.Error("failed to query influx", zap.Error(err))
+	}
+
+	var hc = map[string][][]interface{}{}
+
+	var data = map[time.Weekday]map[int][]float64{}
+
+	if len(resp.Results) > 0 && len(resp.Results[0].Series) > 0 {
+
+		var series = resp.Results[0].Series[0]
+
+		for k := range series.Columns {
+			if k > 0 {
+
+				for _, vv := range series.Values {
+
+					t, err := time.Parse(time.RFC3339, vv[0].(string))
+					if err != nil {
+						logger.Error("casting", zap.Error(err))
+						continue
+					}
+
+					val, err := vv[k].(json.Number).Float64()
+					if err != nil {
+						logger.Error("casting", zap.Error(err))
+						continue
+					}
+
+					if data[t.Weekday()] == nil {
+						data[t.Weekday()] = map[int][]float64{}
+					}
+
+					data[t.Weekday()][t.Hour()] = append(data[t.Weekday()][t.Hour()], val)
+				}
+			}
+		}
+	}
+
+	for day, hours := range data {
+		for hour, vals := range hours {
+			hc["max_pcnt"] = append(hc["max_pcnt"], []interface{}{hour, day, helpers.Max(vals...)})
+		}
+	}
+
+	return c.JSON(hc)
 }
 
 type homeTemplate struct {
