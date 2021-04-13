a9b13f9e109d97a967ca0003b294cb0fa6543367
James Eagle <jimeagle@gmail.com>
2021-04-13 12:46:12 +0100

    Remove any template code

--- webserver.go
+++ webserver.go
@@ -2,23 +2,15 @@ package main
 
 import (
 	"encoding/json"
-	"html/template"
-	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/Jleagle/pure-gym-tracker/helpers"
 	"github.com/Jleagle/pure-gym-tracker/influx"
 	influxquerybuilder "github.com/benjamin658/influx-query-builder"
-	"github.com/go-chi/chi"
-	"github.com/gobuffalo/packr/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cache"
 	"github.com/gofiber/fiber/v2/middleware/compress"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/filesystem"
 	"go.uber.org/zap"
 )
 
@@ -33,12 +25,17 @@ func webserver() {
 	// Routes
 	app.Get("/", rootHandler)
 	app.Get("/heatmap.json", heatmapHandler)
+	app.Post("/submit", submitHandler)
 
 	// Serve
 	err := app.Listen("0.0.0.0:" + os.Getenv("PURE_PORT_BACKEND"))
 	if err != nil {
 		logger.Error("serving webserver", zap.Error(err))
 	}
+}
+
+func rootHandler(c *fiber.Ctx) error {
+	return c.SendString("OK")
 }
 
 func heatmapHandler(c *fiber.Ctx) error {
@@ -102,78 +99,6 @@ func heatmapHandler(c *fiber.Ctx) error {
 	return c.JSON(hc)
 }
 
-type homeTemplate struct {
-	Gym string
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Content-Type", "text/html")
-
-	data := homeTemplate{
-		Gym: chi.URLParam(r, "gym"),
-	}
-
-	err := template.
-		Must(template.ParseFiles("./templates/home.gohtml")).
-		ExecuteTemplate(w, "home", data)
-
-	if err != nil {
-		logger.Error("executing template", zap.Error(err))
-	}
-}
-
-type errorTemplate struct {
-	Code int
-}
-
-func errorHandler(w http.ResponseWriter, _ *http.Request) {
-
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(404)
-
-	data := errorTemplate{
-		Code: 404,
-	}
-
-	err := template.
-		Must(template.ParseFiles("./templates/error.gohtml")).
-		ExecuteTemplate(w, "home", data)
-
-	if err != nil {
-		logger.Error("executing template", zap.Error(err))
-	}
-}
-
-func newGymHandler(w http.ResponseWriter, _ *http.Request) {
-
-	_, _ = w.Write([]byte("new gym"))
-}
-
-func assetHandler(box *packr.Box, path string) func(w http.ResponseWriter, r *http.Request) {
-
-	return func(w http.ResponseWriter, r *http.Request) {
-
-		b, err := box.Find(strings.TrimPrefix(r.URL.Path, path))
-		if err != nil {
-			errorHandler(w, r)
-			return
-		}
-
-		types := map[string]string{
-			".js":  "text/javascript",
-			".css": "text/css",
-			".png": "image/png",
-			".jpg": "image/jpeg",
-		}
-
-		if val, ok := types[filepath.Ext(r.URL.Path)]; ok {
-			w.Header().Add("Content-Type", val)
-		}
-
-		_, err = w.Write(b)
-		if err != nil {
-			logger.Error("writing asset to response", zap.Error(err))
-		}
-	}
+func submitHandler(c *fiber.Ctx) error {
+	return c.SendString("new gym")
 }
