58eab642ca5442d76d290d2a9125f7093210048a
James Eagle <jimeagle@gmail.com>
2021-04-14 13:28:53 +0100

    Update helpers

--- backend/helpers/helpers.go
+++ backend/helpers/helpers.go
@@ -14,3 +14,19 @@ func Max(vars ...float64) float64 {
 	}
 	return max
 }
+
+func Mean(vars ...float64) float64 {
+
+	if len(vars) == 0 {
+		return 0
+	}
+
+	var count float64
+	var sum float64
+
+	for _, i := range vars {
+		count += 1
+		sum += i
+	}
+	return sum / count
+}
