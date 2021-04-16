4a50d228f320a122fcc08d67b2ccf7ac25d6682b
James Eagle <jimeagle@gmail.com>
2021-04-16 21:04:52 +0100

    Tidy chart options

--- frontend/components/BarChart.jsx
+++ frontend/components/BarChart.jsx
@@ -2,63 +2,60 @@ import React from 'react'
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 
-const options = {
-    chart: {
-        type: 'column',
-    },
-    title: {
-        text: '',
-    },
-    credits: {
-        enabled: false,
-    },
-    xAxis: {
-        crosshair: true,
-    },
-    yAxis: [
-        {
-            min: 0,
-            title: {
-                text: 'Members'
-            },
-            labels: {
-                formatter: function () {
-                    return this.value.toLocaleString();
-                },
-            },
-        },
-        {
-            min: 0,
-            max: 100,
-            title: {
-                text: 'Capacity'
-            },
-            labels: {
-                formatter: function () {
-                    return this.value + ' %';
-                },
-            },
-            opposite: true,
-        }
-    ],
-    series: [
-        {
-            name: 'Members',
-            data: [], //Gets overridden
-        },
-        {
-            name: 'Capacity %',
-            data: [], //Gets overridden
-            visible: false,
-        }
-    ]
-}
-
 function BarChart({data}) {
 
-    options.xAxis.categories = Object.keys(data);
-    options.series[0].data = Object.values(data).map(a => a.mean_people[0]);
-    options.series[1].data = Object.values(data).map(a => a.mean_pcnt[0]);
+    const options = {
+        chart: {
+            type: 'column',
+        },
+        title: {
+            text: '',
+        },
+        credits: {
+            enabled: false,
+        },
+        xAxis: {
+            crosshair: true,
+            categories: Object.keys(data),
+        },
+        yAxis: [
+            {
+                min: 0,
+                title: {
+                    text: 'Members'
+                },
+                labels: {
+                    formatter: function () {
+                        return this.value.toLocaleString();
+                    },
+                },
+            },
+            {
+                min: 0,
+                max: 100,
+                title: {
+                    text: 'Capacity'
+                },
+                labels: {
+                    formatter: function () {
+                        return this.value + ' %';
+                    },
+                },
+                opposite: true,
+            }
+        ],
+        series: [
+            {
+                name: 'Members',
+                data: Object.values(data).map(a => a.members),
+            },
+            {
+                name: 'Capacity',
+                data: Object.values(data).map(a => a.percent),
+                visible: false,
+            }
+        ]
+    }
 
     return (<HighchartsReact highcharts={Highcharts} options={options}/>);
 }
