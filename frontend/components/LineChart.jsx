82e305a0c63915cbced159f95db837177741fb27
James Eagle <jimeagle@gmail.com>
2021-04-16 23:34:08 +0100

    Finish line chart

--- frontend/components/LineChart.jsx
+++ frontend/components/LineChart.jsx
@@ -1,17 +1,66 @@
 import React from 'react'
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
+import moment from 'moment';
 
-const options = {
-    title: {
-        text: 'My stock chart'
-    },
-    series: [{
-        data: [1, 2, 3, 2]
-    }]
-}
+function LineChart({data}) {
 
-function LineChart() {
+    const options = {
+        chart: {
+            type: 'spline',
+        },
+        title: {
+            text: ''
+        },
+        credits: {
+            enabled: false,
+        },
+        xAxis: {
+            crosshair: true,
+            categories: data.map(a => moment(a.X * 1000).format("DD MMM @ HH:mm")),
+        },
+        yAxis: [
+            {
+                min: 0,
+                title: {
+                    text: 'Members'
+                },
+                labels: {
+                    formatter: function () {
+                        return this.value.toLocaleString();
+                    },
+                },
+            },
+            {
+                min: 0,
+                max: 100,
+                title: {
+                    text: 'Capacity'
+                },
+                labels: {
+                    formatter: function () {
+                        return this.value + ' %';
+                    },
+                },
+                opposite: true,
+            }
+        ],
+        legend: {
+            verticalAlign: 'bottom',
+        },
+        series: [
+            {
+                name: 'Members',
+                data: data.map(a => a.Y.members),
+            },
+            {
+                name: 'Capacity',
+                data: data.map(a => a.Y.percent),
+                visible: false,
+            }
+        ],
+    };
+
     return (<HighchartsReact highcharts={Highcharts} options={options}/>);
 }
 
