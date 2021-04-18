1859b34ab2278cc97c616016bcc05d0709b5fc56
James Eagle <jimeagle@gmail.com>
2021-04-18 11:49:49 +0100

    Finish heatmap

--- frontend/components/HeatMap.jsx
+++ frontend/components/HeatMap.jsx
@@ -2,44 +2,70 @@ import React from 'react'
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import HighchartsHeatmap from "highcharts/modules/heatmap";
+import moment from "moment";
 
 if (typeof Highcharts === 'object') {
     HighchartsHeatmap(Highcharts);
 }
 
-const chartOptions = {
-    chart: {
-        type: "heatmap",
-    },
-    credits: {
-        enabled: false
-    },
-    title: {
-        text: ''
-    },
-    colorAxis: {
-        minColor: '#FFFFFF',
-        maxColor: '#000'
-    },
-    series: [
-        {
-            name: "Sales per employee",
-            data: [[0, 0, 10], [0, 1, 19], [0, 2, 8], [0, 3, 24], [0, 4, 67], [1, 0, 92], [1, 1, 58], [1, 2, 78], [1, 3, 117], [1, 4, 48], [2, 0, 35], [2, 1, 15], [2, 2, 123], [2, 3, 64], [2, 4, 52], [3, 0, 72], [3, 1, 132], [3, 2, 114], [3, 3, 19], [3, 4, 16], [4, 0, 38], [4, 1, 5], [4, 2, 8], [4, 3, 117], [4, 4, 115], [5, 0, 88], [5, 1, 32], [5, 2, 12], [5, 3, 6], [5, 4, 120], [6, 0, 13], [6, 1, 44], [6, 2, 88], [6, 3, 98], [6, 4, 96], [7, 0, 31], [7, 1, 1], [7, 2, 82], [7, 3, 32], [7, 4, 30], [8, 0, 85], [8, 1, 97], [8, 2, 123], [8, 3, 64], [8, 4, 84], [9, 0, 47], [9, 1, 114], [9, 2, 31], [9, 3, 48], [9, 4, 91]],
-        }
-    ]
-};
-
 function HeatMap({data}) {
 
-    // console.log(data);
-    // chartOptions.series[0].data = data.max_pcnt;
+    const options = {
+        chart: {
+            type: 'heatmap',
+        },
+        title: {
+            text: ''
+        },
+        credits: {
+            enabled: false,
+        },
+        legend: {
+            enabled: false,
+        },
+        colorAxis: {
+            minColor: '#FFFFFF',
+            maxColor: '#2f7ed8'
+        },
+        tooltip: {
+            formatter: function () {
 
-    console.log(data);
+                const day = moment(this.point.y * 60 * 60 * 24 * 1000).format("dddd");
+                return day + ' @ ' + this.point.x + ':00 - ' + this.point.value.toFixed(0) + ' people';
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Day'
+            },
+            labels: {
+                formatter: function () {
+                    return moment(this.value * 60 * 60 * 24 * 1000).format("dddd");
+                },
+            },
+        },
+        xAxis: {
+            labels: {
+                formatter: function () {
+                    return this.value.toLocaleString();
+                },
+            },
+        },
+        series: {
+            name: 'Members',
+            data: data.cols.filter(function (a) {
+                const [y, x] = a.X.split('-');
+                return (Boolean(x) && Boolean(y));
+            }).map(function (a) {
+                const [y, x] = a.X.split('-');
+                return [parseInt(x), parseInt(y), a.Y.members];
+            }),
+        },
+    };
 
     return (
-        <div className="App">
-            <HighchartsReact highcharts={Highcharts} options={chartOptions}/>
-        </div>
+        <HighchartsReact highcharts={Highcharts} options={options}/>
     );
 }
 
