af9bdf8ee7a1d533957ec38d9f2c4b175c716c72
James Eagle <jimeagle@gmail.com>
2021-05-05 20:03:46 +0100

    Update footer

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -53,7 +53,7 @@ function HomePage({yearDay, monthDay, weekDay, weekHour, hour, now}) {
           <h2>By day of the year</h2>
           <BarChart data={yearDay}/>
 
-          <footer>If a gym has 10 or less members inside, it will show as 0.</footer>
+          <footer>Data updated every 10 minutes. If a gym has 10 or less members inside, it will show as 0.</footer>
         </div>
       </div>
     </>
