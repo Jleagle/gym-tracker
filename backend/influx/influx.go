b49f5ffb77a4927b8a1f04fa54b4a492872a4f16
James Eagle <jimeagle@gmail.com>
2021-05-12 20:40:22 +0100

    Save year week

--- backend/influx/influx.go
+++ backend/influx/influx.go
@@ -47,12 +47,15 @@ func Write(gym string, count int, max int, percent float64, t time.Time) (resp *
 		return nil, err
 	}
 
+	_, week := t.ISOWeek()
+
 	return client.Write(influx.BatchPoints{
 		Points: []influx.Point{{
 			Measurement: "gyms",
 			Tags: map[string]string{
 				"gym":      gym,
 				"yearDay":  strconv.Itoa(t.YearDay()),
+				"yearWeek": strconv.Itoa(week),
 				"monthDay": strconv.Itoa(t.Day()),
 				"weekDay":  strconv.Itoa(int(t.Weekday())),
 				"weekHour": strconv.Itoa(int(t.Weekday())) + "-" + strconv.Itoa(t.Hour()),
