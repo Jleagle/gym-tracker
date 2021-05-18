6d1865ffcf977aa0f66c62677ec6c563bd650a4a
James Eagle <jimeagle@gmail.com>
2021-05-18 21:30:28 +0100

    Setup wildcard path

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-    compress: true,
-    future: {
-        webpack5: true,
-    },
-    env:{
-        GYMTRACKER_PORT_BACKEND: process.env.GYMTRACKER_PORT_BACKEND,
-    }
+  compress: true,
+  future: {
+    webpack5: true,
+  },
+  env: {
+    GYMTRACKER_PORT_BACKEND: process.env.GYMTRACKER_PORT_BACKEND,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/all',
+        permanent: true,
+      },
+    ]
+  },
 }
